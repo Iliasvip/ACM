d58183d3c9d888cf93240a7043c46ad30be9d8e0
Ilias Tzoukas <i.tzoukas@outlook.com>
2022-08-26 09:09:18 +0300

    Create Order Repository Class and its Unit Tests

--- ACM.BL/Order.cs
+++ ACM.BL/Order.cs
@@ -22,29 +22,6 @@ namespace ACM.BL
 
 
         /// <summary>
-        /// Retrieve one order.
-        /// </summary>
-        /// <returns></returns>
-        public Order Retrieve(int orderId)
-        {
-            // Code that retrieves the defined order
-
-            return new Order();
-        }
-
-
-        /// <summary>/
-        /// Saves the current order.
-        /// </summary>
-        /// <returns></returns>
-        public bool Save()
-        {
-            // Code that saves the defined order
-
-            return true;
-        }
-
-        /// <summary>
         /// Validates the Order data.
         /// </summary>
         /// <returns></returns>
---
+++ Tests/ACM.BLTest/OrderRepositoryTest.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class OrderRepositoryTest
+    {
+        //-- Arrange: Set up the test
+        //-- Act: Access the member being tested
+        //-- Assert: Determine the result
+
+        [TestMethod]
+        public void RetrieveValid()
+        {
+            //-- Arrange
+            var orderRepository = new OrderRepository();
+            var expected = new Order(10)
+            {
+                OrderDate = new DateTimeOffset(DateTime.Now.Year, 8, 25, 9, 0, 0, new TimeSpan(3, 0, 0))
+            };
+
+            //-- Act
+            var actual = orderRepository.Retrieve(10);
+
+            //-- Assert
+            Assert.AreEqual(expected.OrderDate, actual.OrderDate);
+
+        }
+    }
+}
